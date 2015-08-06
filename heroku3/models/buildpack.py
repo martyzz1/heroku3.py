18e7f15e6513a1083f7cba0442f1ccbbb2cbba28
Colin M. Strickland <cms@beatworm.co.uk>
2015-08-06 16:48:57 +0100

    Builds have an array of buildpacks
    Simple resource object to model buildpack
    Add _array of Buildpack type to buid resource

---
+++ heroku3/models/buildpack.py
@@ -0,0 +1,14 @@
+from . import BaseResource
+
+class Buildpack(BaseResource):
+
+    _strs = [ 'url' ]
+    _pks  = [ 'url' ]
+
+    def __init__(self):
+        super(Buildpack, self).__init__()
+
+    def __repr__(self):
+        return "<buildpack {}'>".format(self.url)
+
+
