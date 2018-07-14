91e8c330a6a5ad6281223b7808409e712fc7e171
Ben <benweatherman@gmail.com>
2018-07-14 13:40:07 -0500

    Add ability to create builds and stream build output
    This is mostly following the example of `Releases`, but with a few build-specific tweaks. Once you create a build it's useful to stream the output so you can see when it finishes:
    
    ```
    build = app.create_build('https://api.github.com/repos/cityofaustin/janis/tarball/master')
    for line in build.stream_output():
      print(line)
    ```
    
    It didn't seem like there was an easy way to add tests for these functions without adding a bunch of plumbing so I skipped that part. Let me know if there's something I should add for tests.

--- heroku3/models/build.py
+++ heroku3/models/build.py
@@ -1,3 +1,5 @@
+import requests
+
 from . import BaseResource, User
 from .buildpack import Buildpack
 from .slug import Slug
@@ -6,7 +8,7 @@ from .sourceblob import SourceBlob
 
 class Build(BaseResource):
     _dates = ['created_at','updated_at']
-    _strs  = ['id','status']
+    _strs  = ['id', 'status', 'output_stream_url', 'stack']
     _pks   = ['id']
     _map   = {'slug': Slug, 'source_blob': SourceBlob, 'user' : User }
     _arrays = { 'buildpacks' : Buildpack }
@@ -16,3 +18,8 @@ class Build(BaseResource):
  
     def __repr__(self):
         return "<build '{0} - {1}'>".format(self.id, self.status)
+
+    def stream_output(self, timeout=None):
+        resp = requests.get(self.output_stream_url, verify=False, stream=True, timeout=timeout)
+        resp.raise_for_status()
+        return resp.iter_lines()
