8a079e766955706fafb06c90a16858a84a037c0e
Martin Moss <martyzz1@gmail.com>
2018-10-14 21:58:25 +0100

    Correct setup.py version

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ required = [
 
 setup(
     name='heroku3',
-    version='3.3.1',
+    version='3.4.0',
     description='Heroku API Wrapper.',
     long_description=open('README.rst').read() + '\n\n' +
                      open('HISTORY.rst').read(),
