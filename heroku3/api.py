a2264d2cf705c44beef9916b9e0b4be201639313
Peter Chien <peter@mobilizeamerica.io>
2018-12-04 15:24:31 -0500

    remove simplejson (#1)

--- heroku3/api.py
+++ heroku3/api.py
@@ -7,13 +7,13 @@ heroku3.api
 This module provides the basic API interface for Heroku.
 """
 
+import json
 import sys
 from pprint import pprint  # noqa
 
 import requests
 from requests.exceptions import HTTPError
 
-from .compat import json
 from .helpers import is_collection
 from .models import Plan, RateLimit
 from .models.account import Account
