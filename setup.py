c633b6ed7c601305803b13a79ecf518eff411806
Martin Moss <martyzz1@gmail.com>
2020-01-23 10:55:48 +0000

    [master] Bump version: 4.0.0 → 4.0.1

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
     author="Martin Moss",
     author_email="martin_moss@btinternet.com",
     url="https://github.com/martyzz1/heroku3.py",
-    download_url="https://github.com/martyzz1/heroku3.py/tarball/v3.4.0",
+    download_url="https://github.com/martyzz1/heroku3.py/tarball/v4.0.1",
     packages=["heroku3"],
     package_data={"": ["LICENSE",]},
     include_package_data=True,
