7a1777fd1e84433bdddd9528e96735ba6e6af2ba
Martin Moss <martyzz1@gmail.com>
2020-01-23 10:13:53 +0000

    fixup annoying versioning issue

--- setup.py
+++ setup.py
@@ -3,11 +3,12 @@
 
 import os
 import sys
+import re
 
 try:
     from setuptools import setup
 except ImportError:
-    from distutils.core import setup # NOQA
+    from distutils.core import setup  # NOQA
 
 
 if sys.argv[-1] == 'publish':
@@ -20,12 +21,25 @@ required = [
 ]
 
 
+def get_version(*file_paths):
+    """Retrieves the version from dj_snap_utils/__init__.py"""
+    filename = os.path.join(os.path.dirname(__file__), *file_paths)
+    version_file = open(filename).read()
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+
+version = get_version("heroku3", "__init__.py")
+
+
 setup(
     name='heroku3',
-    version='3.4.1',
+    version=version,
     description='Heroku API Wrapper.',
-    long_description=open('README.rst').read() + '\n\n' +
-                     open('HISTORY.rst').read(),
+    long_description=open('README.rst').read() + '\n\n'
+    + open('HISTORY.rst').read(),
     author='Martin Moss',
     author_email='martin_moss@btinternet.com',
     url='https://github.com/martyzz1/heroku3.py',
